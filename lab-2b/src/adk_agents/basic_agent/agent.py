da3485f692c55320f43418afc0b8803d42dc0551
Matthew Richmond <matt.richmond@mongodb.com>
2026-07-28 18:19:30 -0400

    Update agent.py

--- lab-2b/src/adk_agents/basic_agent/agent.py
+++ lab-2b/src/adk_agents/basic_agent/agent.py
@@ -1,11 +1,15 @@
-from google.adk.agents.llm_agent import Agent
+from google.adk.agents import Agent
 from google.adk.tools import FunctionTool
+from google.adk.tools.agent_tool import AgentTool
 from .custom_functions import get_fx_rate
+from .custom_agents import google_search_agent
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
-    tools=[FunctionTool(get_fx_rate)]
+    tools=[
+        FunctionTool(get_fx_rate),
+        AgentTool(agent=google_search_agent),
+    ]
 )
