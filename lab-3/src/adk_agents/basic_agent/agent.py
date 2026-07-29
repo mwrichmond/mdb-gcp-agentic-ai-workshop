f8a8a889a13ba71e5174ab4f47760a501e8a1947
Matthew Richmond <matt.richmond@mongodb.com>
2026-07-29 09:34:41 -0400

    Add files via upload

---
+++ lab-3/src/adk_agents/basic_agent/agent.py
@@ -0,0 +1,15 @@
+from google.adk.agents import Agent
+from google.adk.tools import FunctionTool
+from google.adk.tools.agent_tool import AgentTool
+from .custom_functions import get_fx_rate
+from .custom_agents import google_search_agent
+
+root_agent = Agent(
+    model='gemini-2.5-flash',
+    name='root_agent',
+    description='A helpful assistant for user questions.',
+    tools=[
+        FunctionTool(get_fx_rate),
+        AgentTool(agent=google_search_agent)
+    ]
+)
